3acf5eda1a44a79dc1241b47fe44597312c64d3c
Olena Sydorova <olena.sydorova@gmail.com>
2023-05-09 14:57:40 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="Image description"
+      />
+    </a>
+  </li>`;
+    })
+    .join("");
+}
+const GalleryItemMarkup = createGalleryItemsMarkup(galleryItems);
+gallery.innerHTML = GalleryItemMarkup;
 console.log(galleryItems);
