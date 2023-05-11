207de3a10bcebfd7e807c683ff9af75106e904d0
Olena Sydorova <olena.sydorova@gmail.com>
2023-05-11 20:13:29 +0200

    update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="Image description" />
+   </a>
+</li>`;
+    })
+    .join("");
+}
+
+const galleryItemMarkup = createGalleryItemsMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryItemMarkup);
+
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+});
